aee4f6284d965eb075f765140511d01ee0cafe83
heatherzen <heather.zen@gmail.com>
2020-07-17 21:15:30 -0700

    added personal api keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,7 +34,7 @@ function cleanParkInput(parkName) {
     return cleanName.join(" ").toLowerCase();
 }
 
-var apiKey = ""
+var apiKey = "eR6A0aXDvU1BGLeWlqeFkl44fmxkGUIckhcq0fvJ"
 // first fetch to get park code and coordinates
 function getParkData(parkName) {
     var apiUrl = `https://developer.nps.gov/api/v1/parks?q=${parkName}%20national%20park&sort=-name&api_key=${apiKey}`;
@@ -137,7 +137,7 @@ function displayCampgroundInfo(campArray) {
 }
 // daily weather forecast for the next 7 days
 function getParkWeatherData(lat, lon) {
-    var apiWeatherKey = "";
+    var apiWeatherKey = "7d15ba9026d2e440d5540c6e58638b00";
     // plug in coordinates to get data with all needed information
     var apiUrlCoord = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minute&units=imperial&appid=${apiWeatherKey}`
     fetch(apiUrlCoord)
